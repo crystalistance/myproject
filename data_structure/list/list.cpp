54a0c84599199a7f19a6996ac091552c3ba30ce5
crystalistance <crystalistance>
2018-04-15 11:48:53 +0800

    list reverse

---
+++ data_structure/list/list.cpp
@@ -0,0 +1,19 @@
+typedef struct ListNode{
+		struct ListNode *next;
+		int              val;
+};
+
+//链表逆置
+
+ListNode *ReverseList(ListNode *head) {
+		ListNode *pre = NULL, *next;
+
+		while (head != NULL) {
+				next = head->next;
+				head->next = pre;
+				pre = head;
+				head = next;
+		}
+
+		return pre;
+}
